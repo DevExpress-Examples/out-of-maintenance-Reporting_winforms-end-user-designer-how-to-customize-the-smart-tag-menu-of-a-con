7394e2950643680e8683f15bd170cbec28f4d981
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomReportLabelAndDeisgner/CustomXRLblDesigner.vb
@@ -0,0 +1,17 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports DevExpress.XtraReports.Design
+Imports System.ComponentModel.Design
+
+Namespace CustomReportControl
+	Friend Class CustomXRLblDesigner
+		Inherits XRLabelDesigner
+		Public Sub New()
+			MyBase.New()
+		End Sub
+		Protected Overrides Sub RegisterActionLists(ByVal list As DesignerActionListCollection)
+			list.Add(New XRLabelDesignerCustomActionList(Me))
+		End Sub
+	End Class
+End Namespace
